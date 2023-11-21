e5d4e687e30c65a1116943a03889254b42a5b48a
Sergey A <sergalf81@mail.ru>
2023-11-22 01:50:54 +0300

    Создана папка Prog с программой к итоговой работе

---
+++ Prog/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void String_array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+String_array(array1, array2);
+PrintArray(array2);
